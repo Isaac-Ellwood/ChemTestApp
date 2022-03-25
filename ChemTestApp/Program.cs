32b70a88ac114edcb2c30b925426164f7b1a0c1a
Isaac Ellwood <kingisaacellwood@gmail.com>
2022-03-25 15:22:10 +1300

    Adding comments

--- ChemTestApp/Program.cs
+++ ChemTestApp/Program.cs
@@ -1,11 +1,16 @@
+// import modules
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace ChemTestApp
 {
     class Program
     {
+        // main method
         static void Main(string[] args)
         {
+            // define variables
             string chemicalName = "";
             int initialGerms, remainingGerms, time = 1, displayChoice;
             double acidEffect = 0;
@@ -14,11 +19,13 @@ namespace ChemTestApp
             // welcome message
             Console.WriteLine("Welcome to Chemical Test app.\n" +
                 "-------------------------");
-            // 
+            // while loop allows user to input multiple chemicals
             bool flag = true;
             while (flag)
             {
+                // user inputs chemical name
                 chemicalName = StringCheck();
+                // if chemical name == stop, then the while loop will end
                 if (chemicalName == "stop")
                 {
                     flag = false;
@@ -28,21 +35,29 @@ namespace ChemTestApp
                     // loop process 5 times to gather more data
                     for (int i = 0; i < 5; i++)
                     {
+                        // user inputs number of initial germs
                         Console.WriteLine("Enter the number of intitial germs:");
                         initialGerms = NumCheck(1, 1000);
+                        // user inputs number of remaining germs
                         Console.WriteLine("Enter the number of germs left:");
                         remainingGerms = NumCheck(0, initialGerms);
+                        // calculate to find effectiveness of acid * 5
                         acidEffect = +(initialGerms - remainingGerms) / time;
                     }
+                    // calculate average acid effectiveness 
                     acidEffect = acidEffect / 5;
+
+                    // while loop repeats until valid
                     bool flag2 = true;
                     while (flag2)
                     {
+                        // while loop repeats until acidEffect and chemicalName can be added to the SortedList
                         try
                         {
                             chemList.Add(acidEffect, chemicalName);
                             flag2 = false;
                         }
+                        // if the acidEffect and chemicalName cannot be added to the SortedList, an un
                         catch
                         {
                             acidEffect += 0.000000000001;
